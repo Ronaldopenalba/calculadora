26015e7fe976a03ebf193ddad435b00ad6377832
Ronaldo Penalba <ronaldo118@gmail.com>
2021-05-26 11:03:11 -0600

    medificado

--- src/calculadora/rmi.java
+++ src/calculadora/rmi.java
@@ -11,6 +11,7 @@ package calculadora;
  */
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
+import javax.swing.JOptionPane;
 
 
 public class rmi extends UnicastRemoteObject implements calculadora {
@@ -18,21 +19,29 @@ public class rmi extends UnicastRemoteObject implements calculadora {
          int a,b;
          
      } 
+     @Override
      public int sum(int a, int b) throws RemoteException{
          return a +b;
          
      } 
+     @Override
      public int res(int a, int b) throws RemoteException{
          return a - b;
          
      } 
+     @Override
      public int mul(int a, int b) throws RemoteException{
          return a* b;
          
      } 
      public int div(int a , int b) throws RemoteException{
-        return a / b;
-         
+        if( a == 0 || b==0){
+        
+          JOptionPane.showMessageDialog(null, "servidor esta conectado ");
+        }else{
+           return a / b;
+        }
+         return 0;
      } 
 
      
